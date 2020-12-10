0e424ef8af34480191812756beba1777652a64d3
kalyanikapse94 <30695612+KalyaniKapse@users.noreply.github.com>
2020-12-10 17:31:46 +0530

    Asdm test respository

---
+++ 1146_Kalyani_Kapse/server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var path=require("path");
+var fs=require("fs");
+
+
+
+app.use(express.static(path.join(__dirname,'public')));
+
+ app.get('/', function (req, res) {
+   res.sendFile(path.join(___dirname + '/index.html'));
+});
+ 
+
+ 
+var server = app.listen(8181, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://localhost:8181", host, port)
+})
